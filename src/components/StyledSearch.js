6334f1ee2e6c39c8fdb31306707bfccd6223daaa
Roman Brito <romanbrito1@gmail.com>
2018-09-03 09:53:48 -0500

    adding max width to SearchInput and SearchButton

--- src/components/StyledSearch.js
+++ src/components/StyledSearch.js
@@ -6,8 +6,8 @@ export const SearchSection = styled.section.attrs({
 
 export const SearchInput = styled.input.attrs({
   className: "w-100 w-50-ns"
-})``
+})`max-width: 500px;`
 
 export const SearchButton = styled.button.attrs({
-  className: "pointer w-100 w-10-ns"
-})``
+  className: "pointer w-100 w-20-ns"
+})`max-width: 200px;`
